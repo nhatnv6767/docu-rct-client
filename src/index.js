f84dc9c2d49d4abca5b2e80e3759d5b4092d5bd6
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-11 14:48:14 +0900

    index.js import url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './assets/css/boxicon.min.css'
+import './assets/css/flaticon.min.css'
+import './assets/css/boxicon.min.css'
 import './index.css'
+import './assets/css/responsive.min.css'
 
 import { Provider } from 'react-redux'
 import store from './redux/Store'
