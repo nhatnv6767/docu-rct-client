f04924e5187e80b9505e5bfae3fef8f163d457b2
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-11 23:24:57 +0900

    comming soon /pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import ComingSoonArea from './components/About/ComingSoonArea'
 import TopHeader from './components/Layout/TopHeader'
+import CommingSoon from './pages/About/CommingSoon'
 
 function App () {
   return (
     <Router>
       {/*<ComingSoonArea/>*/}
       {/*<TopHeader shippingMessage="Miễn phí vận chuyển với đơn hàng trên 10.000 yên"/>*/}
-      <ComingSoonArea/>
+      <CommingSoon/>
 
     </Router>
   )
---
+++ src/pages/About/CommingSoon.jsx
@@ -0,0 +1,7 @@
+const CommingSoon = (props) => {
+  return (
+    <div>CommingSoon</div>
+  )
+}
+
+export default CommingSoon
