a412762d2f8438da17bf9006d4d43a9ff39dfaac
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-11 23:23:51 +0900

    preloader

---
+++ src/components/Common/Preloader.jsx
@@ -0,0 +1,15 @@
+const Preloader = (props) => {
+  return (
+    <>
+      <div className="preloader">
+        <div className="loader">
+          <div className="sbl-half-circle-spin">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Preloader
