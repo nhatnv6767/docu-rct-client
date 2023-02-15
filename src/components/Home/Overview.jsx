20f888910fbb8222ac98c310dfccb0a49a739d74
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-15 23:48:01 +0900

    Home page overview 1

--- src/components/Home/Overview.jsx
+++ src/components/Home/Overview.jsx
@@ -4,7 +4,15 @@ import { Link } from 'react-router-dom'
 
 const Overview = (props) => {
   return (
-    <div>Overview</div>
+    <section className="overview-area pt-100 pg-20">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-md-6">
+            fdsaf
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
