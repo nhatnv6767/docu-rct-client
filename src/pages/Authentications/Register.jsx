d47b11ccdaa7fb4f23838781ad798b9cdcae8452
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-20 22:47:13 +0900

    Register 1

---
+++ src/pages/Authentications/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageTitle from '../../components/Common/PageTitle'
+
+const Register = (props) => {
+  return (
+    <div className="register-wrapper">
+      <PageTitle title="Đăng ký"/>
+
+    </div>
+  )
+}
+
+export default Register
