80a3254e58cdcb4a7494fa8dd4a608e8fb7f40c5
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-20 22:49:27 +0900

    Register 2 area

---
+++ src/components/Auth/RegisterArea.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterArea = (props) => {
+  return (
+    <div>
+      RegisterArea
+    </div>
+  )
+}
+
+export default RegisterArea
--- src/pages/Authentications/Register.jsx
+++ src/pages/Authentications/Register.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import PageTitle from '../../components/Common/PageTitle'
+import Support from '../../components/Common/Support'
+import Footer from '../../components/Footer/Footer'
 
 const Register = (props) => {
   return (
     <div className="register-wrapper">
       <PageTitle title="Đăng ký"/>
-
+      <section className="register-area ptb-50">
+        <div className="container">
+          Trang dang ky o day
+        </div>
+      </section>
+      <Support/>
+      <Footer/>
     </div>
   )
 }
