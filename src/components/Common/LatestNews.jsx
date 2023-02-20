2b4e1f92b8dba0d337a4b41559d75b58f95c104f
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-20 22:06:43 +0900

    LatestNews 2

--- src/components/Common/LatestNews.jsx
+++ src/components/Common/LatestNews.jsx
@@ -3,11 +3,105 @@ import blog1 from '../../assets/img/blog/blog-1.jpg'
 import blog2 from '../../assets/img/blog/blog-2.jpg'
 import blog3 from '../../assets/img/blog/blog-3.jpg'
 
-const LatestNews = (props) => {
+const LatestNews = ({ paddingClass = '' }) => {
   return (
-    <div>
-      LatestNews
-    </div>
+    <section className={'blog-area ' + paddingClass}>
+      <div className="container">
+        <div className="section-title">
+          <h2>Cập nhật mới nhất</h2>
+        </div>
+
+        <div className="row">
+
+          <div className="col-lg-4 col-md-6">
+            <div className="single-blog">
+              <div className="blog-image">
+                <a href="/blog-details">
+                  <img src={blog1} alt="image"/>
+                </a>
+              </div>
+
+              <div className="blog-content">
+                <span>TMDT</span>
+                <h3>
+                  <a href="/blog-details">
+                    Nhà máy đang sản xuất chip dị động của riêng mình
+                  </a>
+                </h3>
+
+                <div className="post-meta">
+                  <a href="#">Admin</a> / 18-05-2023
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                  aut corporis deleniti doloremque earum eveniet ex nam
+                  perferendis porro sapiente suscipit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-4 col-md-6">
+            <div className="single-blog">
+              <div className="blog-image">
+                <a href="/blog-details">
+                  <img src={blog2} alt="image"/>
+                </a>
+              </div>
+
+              <div className="blog-content">
+                <span>TMDT</span>
+                <h3>
+                  <a href="/blog-details">
+                    Nhiều mẫu đồng hồ thông minh đẹp, thời trang
+                  </a>
+                </h3>
+
+                <div className="post-meta">
+                  <a href="#">Admin</a> / 18-03-2023
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                  aut corporis deleniti doloremque earum eveniet ex nam
+                  perferendis porro sapiente suscipit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-lg-4 col-md-6">
+            <div className="single-blog">
+              <div className="blog-image">
+                <a href="/blog-details">
+                  <img src={blog3} alt="image"/>
+                </a>
+              </div>
+
+              <div className="blog-content">
+                <span>TMDT</span>
+                <h3>
+                  <a href="/blog-details">
+                    Đồ điện gia dụng cũng đã về tại shop
+                  </a>
+                </h3>
+
+                <div className="post-meta">
+                  <a href="#">Admin</a> / 18-03-2023
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                  aut corporis deleniti doloremque earum eveniet ex nam
+                  perferendis porro sapiente suscipit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
