1777fa41c7dad3c2a072466c482eb6559fef504e
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-20 22:33:25 +0900

    Partner 2

--- src/components/Common/Partner.jsx
+++ src/components/Common/Partner.jsx
@@ -8,12 +8,64 @@ import partner3 from '../../assets/img/partner/partner-3.png'
 import partner4 from '../../assets/img/partner/partner-4.png'
 import partner5 from '../../assets/img/partner/partner-5.png'
 
-const Partner = ({ paddingClass = "" }) => {
+const Partner = ({ paddingClass = '' }) => {
 
-  const options = {}
+  const options = {
+    loop: true,
+    margin: 0,
+    nav: false,
+    mouseDrag: false,
+
+    items: 5,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 500,
+    responsive: {
+      0: { items: 2 },
+      600: { items: 2 },
+      1000: { items: 4 },
+    },
+  }
   return (
-    <div>
-      Partner
+    <div className={'partner-area' + paddingClass}>
+      <div className="container">
+        <OwlCarousel
+          className="owl-theme"
+          margin={20}
+          {...options}
+        >
+          <div className="partner-item">
+            <a href="#">
+              <img src={partner1} alt="image"/>
+            </a>
+          </div>
+
+          <div className="partner-item">
+            <a href="#">
+              <img src={partner2} alt="image"/>
+            </a>
+          </div>
+
+          <div className="partner-item">
+            <a href="#">
+              <img src={partner3} alt="image"/>
+            </a>
+          </div>
+
+          <div className="partner-item">
+            <a href="#">
+              <img src={partner4} alt="image"/>
+            </a>
+          </div>
+
+          <div className="partner-item">
+            <a href="#">
+              <img src={partner5} alt="image"/>
+            </a>
+          </div>
+
+        </OwlCarousel>
+      </div>
     </div>
   )
 }
