319878da908882a143a1ee57a9aa1dcd55270ff6
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-20 22:40:19 +0900

    Home config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import BestSellers from '../../components/Common/BestSellers'
 import LatestNews from '../../components/Common/LatestNews'
 
 import { useDispatch, useSelector } from 'react-redux'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import cartContext from '../../contexts/cart-context'
 import Banner from '../../components/Home/Banner'
 import './Home.css'
@@ -24,6 +24,40 @@ const Home = (props) => {
   const dispatch = useDispatch()
   const { products } = useSelector(state => state.productReducer)
   const context = useContext(cartContext)
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  }, [])
+
+  const showQuickView = (product) => {
+    setIsOpen(true)
+    setProduct(product)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+    setProduct({})
+  }
+
+  const addToCart = (product) => {
+    let currentItem = {
+      _id: product._id,
+      name: product.name,
+      price: product.price,
+      type: product.type,
+      total_in_stock: product.total_in_stock,
+      image_public_id: product.image_public_id,
+      quantity,
+    }
+    context.addItemToCart(currentItem)
+  }
+
   return (
     <>
 
