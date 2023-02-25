a767dd8bb6b059ec9403a7e0d0b0bf349be55659
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-25 14:03:18 +0900

    ProductArea 2

--- src/components/Products/ProductsArea.jsx
+++ src/components/Products/ProductsArea.jsx
@@ -2,22 +2,21 @@ import React, { useContext, useEffect, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import { Image } from 'cloudinary-react'
 import axios from 'axios'
-import authContext from "../../contexts/auth-context"
-import { logDOM } from '@testing-library/react'
+import authContext from '../../contexts/auth-context'
 
 const ProductsArea = ({ products, history, editProduct, deleteProduct }) => {
 
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
   const [image, setImage] = useState(null)
-  const [type, setType] = useState("")
-  const [color, setColor] = useState("")
-  const [price, setPrice] = useState("")
-  const [inStock, setInStock] = useState("")
-  const [role, setRole] = useState("user")
+  const [type, setType] = useState('')
+  const [color, setColor] = useState('')
+  const [price, setPrice] = useState('')
+  const [inStock, setInStock] = useState('')
+  const [role, setRole] = useState('user')
   const [product, setProduct] = useState({})
   const [imagePublicId, setImagePublicId] = useState(null)
-  const [product_images, setProductImages] = useState("")
+  const [product_images, setProductImages] = useState('')
   const context = useContext(authContext)
 
   useEffect(() => {
@@ -27,6 +26,27 @@ const ProductsArea = ({ products, history, editProduct, deleteProduct }) => {
       .then((res) => setRole(res.data.role))
       .catch((err) => console.log(err))
   }, [])
+
+  const goToDetails = (id) => {
+    history.push(`/products-details/${id}`)
+  }
+
+  const openDeleteModal = (product, imagePublicId) => {
+    setProduct(product)
+    setImagePublicId(imagePublicId)
+  }
+
+  const openEditModal = (product, imagePublicId) => {
+    setProduct(product)
+    setName(product.name)
+    setDescription(product.description)
+    setProductImages(product.image_public_id)
+    setType(product.type)
+    setColor(product.color)
+    setPrice(product.price)
+    setInStock(product.total_in_stock)
+    setImagePublicId(imagePublicId)
+  }
 
   return (
     <div>
