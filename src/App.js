410821c8877c5fbcc63148b974b6fe370d94b76f
nhatnv6767 <nhatnvse90183@gmail.com>
2023-02-26 23:15:17 +0900

    ProductArea 3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Support from './components/Common/Support'
 import NewArrivals from './components/Common/NewArrivals'
 import Home from './pages/Home/Home'
 import Register from './pages/Authentications/Register'
+import ProductsArea from './components/Products/ProductsArea'
 
 function App () {
   return (
@@ -17,10 +18,13 @@ function App () {
           shippingMessage="Miễn phí vận chuyển với đơn hàng trên 10.000 &#165;"/>
         <MiddleHeader/>
         <Navbar/>
+        <ProductsArea/>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/register" component={Register}/>
         </Switch>
+
+
       </Router>
     </div>
   )
--- src/components/Products/ProductsArea.jsx
+++ src/components/Products/ProductsArea.jsx
@@ -48,11 +48,168 @@ const ProductsArea = ({ products, history, editProduct, deleteProduct }) => {
     setImagePublicId(imagePublicId)
   }
 
+  const handleChange = async e => {
+    const image = e.target.files[0]
+    const formData = new FormData()
+    formData.append('file', image)
+    formData.append('upload_preset', 'econix')
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    }
+
+    const { data } = await axios.post(
+      `https://api.cloudinary.com/v1_1/dev-empty/image/upload`,
+      formData,
+      config,
+    )
+    setProductImages(data.public_id)
+  }
+
   return (
-    <div>
-      ProductsArea
+    <div className="products-area-wrap container">
+      <>
+        <div className="container">
+          <table className="order_list_table product_list_table">
+            <thead>
+            <tr className="order_table100_head">
+              <th className="order_column1">Ảnh</th>
+              <th className="order_column1">Tên</th>
+              <th className="order_column1">Hành động</th>
+            </tr>
+            </thead>
+            <tbody>
+            {products &&
+              products.map((product, index) => {
+                return (
+                  <tr key={product._id} className="product_tr">
+                    <td className="order_column1">
+                      <Image
+                        key={index}
+                        cloudName={process.env.REACT_APP_CLOUDINARY_NAME}
+                        publicId={product.image_public_id}
+                        width="50"
+                        crop="scale"
+                      />
+                    </td>
+                    <td
+                      onClick={() => goToDetails(product._id)}
+                      style={{ cursor: 'pointer' }}
+                      className="order_column2"
+                    >
+                      {product.name}
+                    </td>
+
+                    <td className="order_column4">
+                      <button
+                        type="button"
+                        onClick={() => openDeleteModal(product,
+                          product.imageId)}
+                        className="btn btn-danger"
+                        data-toggle="modal"
+                        data-target="#deleteModal"
+
+                      >
+                        Xoá
+                      </button>
+                      {' '}
+                      <button
+                        className="btn btn-primary"
+                        data-toggle="modal"
+                        data-target="#editModal"
+                        onClick={() => openEditModal(product, product.imageId)}
+                      >
+                        Sửa
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+            </tbody>
+          </table>
+        </div>
+
+        <div
+          className="modal fade"
+          id="deleteModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="deleteModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="deleteModalLabel">
+                  Xoá - {product.name}
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+
+              <div className="modal-body">
+                Bạn có chắc chắn muốn xoá?
+              </div>
+
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-dismiss="modal"
+                >
+Huỷ bỏ
+                </button>
+                <button
+                type="button"
+                  className="btn btn-danger"
+                data-dismiss="modal"
+                onClick={() => deleteProduct(product._id, imagePublicId}
+                >
+                  Xoá
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="modal fade"
+          id="editModal"
+          tabIndex="-1"
+          role="dialog"
+          aria-labelledby="editModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="editModalLabel">
+                  Sửa - {name}
+                </h5>
+
+                <button
+                type="button"
+                  className="close"
+                >
+
+                </button>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </>
     </div>
   )
 }
 
-export default ProductsArea
+export default withRouter(ProductsArea)
