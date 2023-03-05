65a1d6ddcf42fbf7773af169bee186393f6de1b9
nhatnv6767 <nhatnvse90183@gmail.com>
2023-03-05 09:07:12 +0900

    LoginArea __

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App () {
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/register" component={Register}/>
-          <Route path="/login" component={Login}/>
+          {/*<Route path="/login" component={Login}/>*/}
           <Route path="/products" component={Products}/>
         </Switch>
 
--- src/components/Auth/LoginArea.jsx
+++ src/components/Auth/LoginArea.jsx
@@ -37,10 +37,46 @@ const LoginArea = ({ customClass = '', history }) => {
   }
 
   return (
-    <div>
-      LoginArea
+    <div className={'login-form' + customClass}>
+      {alertMsg &&
+        (Array.isArray(alertMsg) ? (
+          <div className="alert alert-danger" role="alert">
+            <ul className="errors" style={{ marginBottom: 0 }}>
+              {alertMsg.map((msg) => (
+                <li key={msg} className="error">
+                  {msg}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div className={`alert alert-success`} role="alert">
+            {alertMsg}
+          </div>
+        ))
+      }
+
+      <h2>Đăng nhập</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <input type="text"
+                 className="form-control"
+                 placeholder="Email"
+                 ref={email}
+          />
+        </div>
+        <div className="form-group">
+          <input type="password"
+                 className="form-control"
+                 placeholder="Mật khẩu"
+                 ref={password}
+          />
+        </div>
+
+
+      </form>
     </div>
   )
 }
 
-export default LoginArea
+export default withRouter(LoginArea)
